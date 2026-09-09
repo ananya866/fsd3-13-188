5cdbf47ee9ed56900b09e471834eabad28004834
Ananya singh <>
2026-09-09 11:44:51 +0530

    prg1.js

--- lab3/prg4.js
+++ lab3/prg4.js
@@ -1,26 +1,25 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
+  const products = {
+    id: 1,
+    name: "Mobile",
+    price: 25000,
+    rating: 4.5,
+    review: 200,
+  };
+
   if (req.url === "/api/products") {
-    res.end(
-      JSON.stringify({
-        id: 1,
-        name: "Mobile",
-        price: 25000,
-        rating: 4.5,
-        review: 200,
-      }),
-    );
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
 
+    res.end(JSON.stringify(products));
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
 
-    
-  }eslse{
-    res.statusCode=404;
-    res.end();
+    res.end("404 - Page Not Found");
   }
-  
-  
-
 });
 
 server.listen(3000, () => {
