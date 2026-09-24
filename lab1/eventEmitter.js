f2eb77862d6c5c83ce427271b2c73cc91ca991d8
Ananya singh <>
2026-09-24 21:41:10 +0530

    update eventemitter

--- lab1/eventEmitter.js
+++ lab1/eventEmitter.js
@@ -1,18 +1,26 @@
 import { EventEmitter } from "node:events";
-
 const task = new EventEmitter();
 
 const sayHi = (name) => {
-  console.log(`Logged In to ${name}`);
+  console.log(`Logged In ${name}`);
 };
+
 const starts = () => {
-  console.log("System started");
+  console.log("System Started");
 };
-task.on("greet", starts); //event and method binding
-task.on("greet", sayHi);
-task.on("greet",(name))
 
-task.emit("greet", "Anil Pandey"); //announcement
+task.once("greet", starts);
+task.on("greet", sayHi); //event method
+task.on("greet", (name) => {
+  console.log(`${name} starts shopping`);
+});
 
-task.emit("greet", "Rahul singh");
+task.on("greet", (name) => {
+  console.log(`${name} logged out`);
+});
 
+task.emit("greet", "Anshika"); //Announcement
+task.emit("greet", "Palak");
+task.emit("greet", "Mahi");
+
+eventemitter;
