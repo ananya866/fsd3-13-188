2e348224b4528dfa790bef18aa73f873a9777a2f
Ananya singh <>
2026-09-24 13:03:06 +0530

    update file3

--- lab2/file3.js
+++ lab2/file3.js
@@ -1,17 +1,28 @@
-import { readFile, writeFile, appendFile } from "fs/promises";
-const writeData = async (fname, content) => {
-  await writeFile(fname, content);
-  console.log("File written");
+import { readFile, writeFile, appendFile, unlink } from "fs/promises";
+
+const writeData = async (fname, contents) => {
+  await writeFile(fname, contents);
+  console.log("File Written");
 };
+
 const readData = async (fname) => {
   const data = await readFile(fname, "utf-8");
   console.log("File contents");
   console.log(data);
 };
-const appendData = async (fname, content) => {
-  await appendFile(fname, "\n"+ content);
+
+const appendData = async (fname, contents) => {
+  await appendFile(fname, "\n" + contents);
 };
-await writeData("happy.txt", "I am very happy");
+
+const deleteFile = async (fname) => {
+  await unlink(fname);
+  console.log(`${fname} deleted`);
+};
+
+/*await writeData("happy.txt", "I am very happy.");
 await readData("happy.txt");
 await appendData("happy.txt", "FSD is interesting");
-await readData("happy.txt");
+await readData("happy.txt");*/
+
+await deleteFile("happy.txt");
